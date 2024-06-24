cab7ebc6b0de325a2222b574fde2ac62ca0c89a9
gmribeiro01 <gabriel.moraes@philips.com>
2024-06-24 13:16:50 -0300

    Funcoes para operacoes

---
+++ M05.java
@@ -0,0 +1,21 @@
+public class M05 {
+    public static void main(String[] args) {
+
+    }
+
+    public double addition(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public double subtraction(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public double multiplication(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public double division(double num1, double num2) {
+        return num1 / num2;
+    }
+}
